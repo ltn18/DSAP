300825b17288c68ac0c0bb5c29f47cf6c8758a26
HOANGLAM2952002 <tommy2952002@gmail.com>
2021-05-12 06:05:41 +0700

    first commit

---
+++ cmake-build-debug/CMakeFiles/Competitive.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Competitive.dir/generation.cpp.obj"
+  "Competitive.exe"
+  "Competitive.exe.manifest"
+  "Competitive.pdb"
+  "libCompetitive.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Competitive.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
