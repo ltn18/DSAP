18c2e018e9bb1144e32864dc2258dac7c25dc393
HOANGLAM2952002 <tommy2952002@gmail.com>
2021-05-12 06:27:19 +0700

    binary_string_gen

--- Generation/binary_string_gen.cpp
+++ Generation/binary_string_gen.cpp
@@ -1,10 +1,18 @@
-// author: Lam Nguyen
-
 /**
+ * @author: Lam Nguyen
+ *
+ * @abstract:
  * Binary string of length n is a binary representation of
  * p(x) in [0, 2^n-1]. The number of binary strings = 2^n.
+ *
+ * @description:
  * This program will list all binary string representations
  * of numbers in range [0, 2^n-1].
+ *
+ * @idea:
+ * - Find the first '0' from end, replace that with '1'
+ * - Replace all characters after that found '0' to be '0'
+ * - If not found '0' -> no more generation
  */
 
 #include <bits/stdc++.h>
